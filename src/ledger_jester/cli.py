8693b3a8a24caf7f18f42ef9118452b7098c9d88
octvs <octvs@posteo.de>
2025-10-13 22:50:06 +0200

    feat: further simplication

--- src/ledger_jester/cli.py
+++ src/ledger_jester/cli.py
@@ -1,55 +1,20 @@
 import argparse
 import logging
 import os
-import os.path
 import re
 import sys
+from pathlib import Path
 
-from ledger_jester import LedgerAutosyncException
 from ledger_jester.ledgerwrap import Ledger
 from ledger_jester.sync import CsvSynchronizer
 
 
-def find_ledger_file(ledgerrcpath=None):
-    """Returns main ledger file path or raise exception if it cannot be \
-found."""
-    if ledgerrcpath is None:
-        ledgerrcpath = os.path.abspath(os.path.expanduser("~/.ledgerrc"))
+def find_ledger_file():
     if "LEDGER_FILE" in os.environ:
-        return os.path.abspath(os.path.expanduser(os.environ["LEDGER_FILE"]))
-    elif os.path.exists(ledgerrcpath):
-        # hacky
-        ledgerrc = open(ledgerrcpath)
-        for line in ledgerrc.readlines():
-            md = re.match(r"--file\s+([^\s]+).*", line)
-            if md is not None:
-                return os.path.abspath(os.path.expanduser(md.group(1)))
+        return Path(os.getenv("LEDGER_FILE")).expanduser().absolute()
     else:
+        logging.warn("Please define LEDGER_FILE env var!")
         return None
-
-
-def print_results(converter, ofx, ledger, txns, args):
-    """
-    This function is the final common pathway of program:
-
-    Print initial balance if requested;
-    Print transactions surviving de-duplication filter;
-    Print balance assertions if requested;
-    Print commodity prices obtained from position statements
-    """
-
-    for txn in txns:
-        print(converter.convert(txn).format(args.indent))
-    if args.assertions:
-        print(converter.format_balance(ofx.account.statement))
-
-    # if OFX has positions use these to obtain commodity prices
-    # and print "P" records to provide dated/timed valuations
-    # Note that this outputs only the commodity price,
-    # not your position (e.g. # shares), even though this is in the OFX record
-    if hasattr(ofx.account.statement, "positions"):
-        for pos in ofx.account.statement.positions:
-            print(converter.format_position(pos))
 
 
 def import_csv(ledger, args):
@@ -63,8 +28,6 @@ def import_csv(ledger, args):
     txns = sync.parse_file(
         args.PATH, accountname=args.account, unknownaccount=args.unknownaccount
     )
-    if args.reverse:
-        txns = reversed(txns)
     for txn in txns:
         if txn is not None:
             print(txn.format(args.indent, args.assertions))
@@ -101,14 +64,6 @@ if importing from file, set account name for import",
         type=str,
         default=None,
         help="specify rule file to READ for Payee matching",
-    )
-    parser.add_argument(
-        "-L",
-        "--no-ledger",
-        dest="no_ledger",
-        action="store_true",
-        default=False,
-        help="do not de-duplicate against a ledger file",
     )
     parser.add_argument(
         "-i",
@@ -150,12 +105,6 @@ found by payee",
         between {}.""",
     )
     parser.add_argument(
-        "--reverse",
-        action="store_true",
-        default=False,
-        help="print CSV transactions in reverse order",
-    )
-    parser.add_argument(
         "-y",
         "--date-format",
         type=str,
@@ -167,34 +116,20 @@ found by payee",
     args = parser.parse_args(args)
 
     ledger_file = None
-    if args.ledger and args.no_ledger:
-        raise LedgerAutosyncException(
-            "You cannot specify a ledger file and -L"
-        )
-    elif args.ledger:
+    if args.ledger:
         ledger_file = args.ledger
     else:
         ledger_file = find_ledger_file()
+
     if args.debug:
         logging.basicConfig(level=logging.DEBUG)
 
-    if ledger_file is None:
-        sys.stderr.write(
-            "LEDGER_FILE environment variable not set, and no \
-.ledgerrc file found, and -l argument was not supplied: running with deduplication disabled. \
-All transactions will be printed!\n"
-        )
-        ledger = None
-    elif args.no_ledger:
-        ledger = None
-    else:
-        ledger = Ledger(ledger_file=ledger_file, no_pipe=True)
+    ledger = Ledger(ledger_file=ledger_file, no_pipe=True)
 
-    if args.rules and os.path.exists(args.rules):
+    if args.rules and Path(args.rules).exists():
         with open(args.rules) as f:
             for line in f:
                 regex, account = line.strip().split("\t")
                 ledger.add_rule(re.compile(regex, re.IGNORECASE), account)
 
-    _, file_extension = os.path.splitext(args.PATH.lower())
     import_csv(ledger, args)
