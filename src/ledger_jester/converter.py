5ef8e34cfad5160695876c1c5c79f1a876515e9b
octvs <octvs@posteo.de>
2025-10-16 01:54:15 +0200

    feat: add support for amazon visa credit card

--- src/ledger_jester/converter.py
+++ src/ledger_jester/converter.py
@@ -321,3 +321,65 @@ class RevolutConverter(CsvConverter):
             payee=payee,
             postings=postings,
         )
+
+
+class AmazonVisaConverter(CsvConverter):
+    FIELDSET = set(
+        [
+            "Datum",
+            "Zeit",
+            "Karte",
+            "Beschreibung",
+            "Umsatzkategorie",
+            "Unterkategorie",
+            "Betrag",
+            "Punkte",
+        ]
+    )
+
+    def __init__(self, *args, **kwargs):
+        super(AmazonVisaConverter, self).__init__(*args, **kwargs)
+        if self.name is None:
+            self.name = "Liabilities:CreditCard:Amazon"
+
+    def convert(self, row):
+        # TODO: Currently we don't handle Amazon Points on "Punkte" col
+        if row is None:
+            return None
+
+        date = dt.strptime(row["Datum"], "%d.%m.%Y ")
+        _amount, _curr = row["Betrag"].split()
+        amount = Decimal(_amount.replace(",", "."))
+        currency = self.mk_currency(_curr)
+        meta = {"csvid": self.get_csv_id(row)}
+        payee = self.lgr.get_autosync_payee(row["Beschreibung"], self.name)
+        acct_dst = self.mk_dynamic_account(payee, exclude=self.name)
+
+        posting_dst = Posting(
+            account=acct_dst,
+            amount=Amount(amount, currency, reverse=True),
+        )
+        posting_src = Posting(
+            account=self.name,
+            amount=Amount(amount, currency),
+            metadata=meta,
+        )
+        postings = [posting_dst, posting_src]
+
+        return Transaction(
+            date=date,
+            cleared=True,
+            date_format="%Y/%m/%d",
+            payee=payee,
+            postings=postings,
+        )
+
+    @staticmethod
+    def mk_currency(currency):
+        if currency == "$":
+            currency = "USD"
+        elif currency == "£":
+            currency = "GBP"
+        elif currency == "€":
+            currency = "EUR"
+        return currency
