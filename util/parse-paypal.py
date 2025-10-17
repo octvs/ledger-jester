c4a48e6e027afe056e9db93e3c264dce3ed535a8
octvs <octvs@posteo.de>
2025-10-17 17:37:56 +0200

    feat: add util script for paypal

---
+++ util/parse-paypal.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env nix
+#! nix shell github:tomberek/-#python3With.pandas nixpkgs#xan --command python
+
+import sys
+from pathlib import Path
+
+import pandas as pd
+
+fpath = Path(sys.argv[1])
+if fpath.suffix != ".csv":
+    print(f"Unsupported file extension provided: {fpath.suffix}")
+    exit()
+
+df = pd.read_csv(fpath)
+df["dt"] = pd.to_datetime(df["Date"] + df["Time"], format="%d/%m/%Y%H:%M:%S")
+for _, month in df.groupby(pd.Grouper(key="dt", freq="ME")):
+    if month.empty:
+        continue
+    dt = month["dt"].reset_index(drop=True)[0].strftime("%Y%m")
+    fname = f"out/{dt}-paypal.csv"
+    if Path(fname).exists():
+        print("File already exists!")
+        exit()
+    month = month.sort_values(by=["Date", "Time"])
+    month = month.drop("dt", axis=1).to_csv(fname, index=False)
