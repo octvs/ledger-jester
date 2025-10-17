8c790bf330ddcd80338471ffca73f833267dff8f
octvs <octvs@posteo.de>
2025-10-17 17:37:43 +0200

    feat: move util outputs to a single dir

--- util/parse-enpara.py
+++ util/parse-enpara.py
@@ -20,5 +20,5 @@ df = pd.read_csv(fpath).dropna(how="all")
 df["dt"] = pd.to_datetime(df["Tarih"], format="%d.%m.%Y")
 for _, month in df.groupby(pd.Grouper(key="dt", freq="ME")):
     dt = month["dt"].reset_index(drop=True)[0].strftime("%Y%m")
-    fname = f"{dt}-enpara.csv"
+    fname = f"out/{dt}-enpara.csv"
     month = month.drop("dt", axis=1).to_csv(fname, index=False)
--- util/parse-revolut.py
+++ util/parse-revolut.py
@@ -15,6 +15,6 @@ df = pd.read_csv(fpath)
 df["dt"] = pd.to_datetime(df["Completed Date"], format="%Y-%m-%d %H:%M:%S")
 for _, month in df.groupby(pd.Grouper(key="dt", freq="ME")):
     dt = month["dt"].reset_index(drop=True)[0].strftime("%Y%m")
-    fname = f"{dt}-revolut.csv"
+    fname = f"out/{dt}-revolut.csv"
     month = month.sort_values(by=["Completed Date", "Product"])
     month = month.drop("dt", axis=1).to_csv(fname, index=False)
