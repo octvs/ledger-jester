5170e957f7b9a136f31fa23059afa6db12f14e2f
octvs <octvs@posteo.de>
2025-12-05 16:27:16 +0100

    build: flake: migrate to flake-parts

--- flake.nix
+++ flake.nix
@@ -3,30 +3,26 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    systems.url = "github:nix-systems/default";
   };
 
-  outputs = {
-    nixpkgs,
-    flake-utils,
-    ...
-  }:
-    flake-utils.lib.eachDefaultSystem (
-      system: let
-        pkgs = nixpkgs.legacyPackages.${system};
-        runtimeDeps = with pkgs.python3Packages; [ledger];
-        buildDeps = with pkgs.python3Packages; [setuptools];
-        devDeps = with pkgs.python3Packages; [pandas pandas-stubs xlrd tqdm];
+  outputs = inputs:
+    inputs.flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = import inputs.systems;
+      perSystem = {pkgs, ...}: let
+        dependencies = with pkgs.python3.pkgs; [ledger] ++ [pkgs.ledger];
+        # devDeps = with pkgs.python3Packages; [pandas pandas-stubs xlrd tqdm];
       in {
         packages.default = pkgs.python3Packages.buildPythonApplication {
           pname = "ledger-jester";
-          version = "0.1";
+          version = "0-unstable";
           pyproject = true;
-          dependencies = runtimeDeps ++ buildDeps;
           src = ./.;
+          build-system = with pkgs.python3.pkgs; [setuptools];
+          inherit dependencies;
           dontCheckRuntimeDeps = true;
         };
-        devShells.default = pkgs.mkShell {buildInputs = runtimeDeps ++ devDeps;};
-      }
-    );
+      };
+    };
 }
