3ca1800ce62f6f41f37693b6175175d6edacb111
octvs <octvs@posteo.de>
2026-01-09 18:20:39 +0100

    feat: parsers/enpara: deduce account type auto
    Check a relevant cell from the export xls file to deduce whether it is
    the savings account or not.

--- src/parsers/enpara.py
+++ src/parsers/enpara.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+import logging
+
 import pandas as pd
 
 from parsers import Parser
@@ -12,7 +14,23 @@ class EnparaParser(Parser):
 
     TYPE = "enpara"
 
+    def __init__(self):
+        subtype = None
+
+    def deduce_account_type(self, fpath):
+        acc_type = pd.read_excel(fpath, usecols=[1, 4]).iat[1, 1]
+        if acc_type == "Vadesiz TL":
+            self.subtype = ""
+        elif acc_type == "Birikim TL":
+            self.subtype = "sav"
+        else:
+            logging.warning(
+                f"Found {acc_type} on cell for account type, which is not recognized, can't recover."
+            )
+            exit()
+
     def read_file(self, fpath) -> pd.DataFrame:
+        self.deduce_account_type(fpath)
         df1 = pd.read_excel(
             fpath,
             header=10,
@@ -28,5 +46,5 @@ class EnparaParser(Parser):
 
     def parse_groups(self, group):
         dt = group["Tarih"].reset_index(drop=True)[0].strftime("%Y%m")
-        fname = f"out/{dt}-enpara.csv"
+        fname = f"out/{dt}-enpara{self.subtype}.csv"
         group.to_csv(fname, index=False)
