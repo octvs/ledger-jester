dc0148963889e77942f13198a765e645cccf791a
octvs <octvs@posteo.de>
2026-07-14 23:05:51 +0200

    build: initialize repo w/ project build files

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "A stripped down fork of ledger-autosync";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    systems.url = "github:nix-systems/default";
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = inputs:
+    inputs.flake-parts.lib.mkFlake {inherit inputs;} {
+      imports = [inputs.treefmt-nix.flakeModule];
+      systems = import inputs.systems;
+      perSystem = {pkgs, ...}: {
+        packages.default = pkgs.python3Packages.buildPythonApplication {
+          pname = "ledger-jester";
+          version = "0-unstable";
+          pyproject = true;
+          src = ./.;
+          build-system = with pkgs.python3.pkgs; [setuptools];
+          dependencies = [pkgs.ledger];
+        };
+        treefmt = {
+          projectRootFile = "flake.nix";
+          programs.alejandra.enable = true;
+        };
+      };
+    };
+}
