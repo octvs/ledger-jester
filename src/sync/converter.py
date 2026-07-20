614c192933ef45db51ea8ffe72fe464ff0c4ec6b
octvs <octvs@posteo.de>
2026-07-20 18:17:19 +0200

    perf: converters: do not init unnnecessarily
    Do not initialize the converters unnecessarily to abstain from extra
    call to __init__. In case of revolut, this creates a subprocess, which
    is very costly.
    
    This reduced runtime from 0.25s to 0.13s on my test.

--- src/sync/converter.py
+++ src/sync/converter.py
@@ -8,6 +8,6 @@ DOMAIN = "converters"
 def converter_factory(fieldset: list) -> object:
     """Provide the fitting converter based on columns of the csv file. TODO."""
     for _type, klass in REGISTRY[DOMAIN].items():
-        if set(klass("none").COLS.values()) <= set(fieldset):
+        if set(klass.COLS.values()) <= set(fieldset):
             return get(DOMAIN, _type)
     raise NotImplementedError
