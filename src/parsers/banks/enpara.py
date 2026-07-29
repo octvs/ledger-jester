08e51783955f8bd1ad2d304717e0cfb88ac499cc
octvs <octvs@posteo.de>
2026-07-29 14:24:53 +0200

    feat: parsers: add enpara support

---
+++ src/parsers/banks/enpara.py
@@ -0,0 +1,30 @@
+"""Parser for Enpara XLS exports."""
+
+from io import BytesIO
+from pathlib import Path
+from typing import override
+
+import pandas as pd
+
+from parsers import REGISTRY, Parser
+
+
+@REGISTRY.register
+class EnparaParser(Parser):
+    """Parser class for Enpara XLS exports."""
+
+    TYPE = "enpara"
+    FTYPE = "xls"
+    SUBTYPES = {"Vadesiz TL": "", "Birikim TL": "sav"}
+
+    @override
+    def read_file(self, fpath: Path) -> pd.DataFrame:
+        """Read a Enpara XLS export and return a DataFrame."""
+        content = BytesIO(fpath.read_bytes())
+        meta_df = pd.read_excel(content, header=None, nrows=3, usecols=[4])
+        self.assign_subtype_suffix(meta_df.iat[2, 0])
+        df = pd.read_excel(
+            content, header=10, usecols=[1, 2, 5, 7, 8], skipfooter=4
+        )
+        df["dt"] = pd.to_datetime(df["Tarih"], format="%d/%m/%Y")
+        return df
