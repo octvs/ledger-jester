7fa86c4a62d6030daa9262e2e1745a5fe8280a68
octvs <octvs@posteo.de>
2026-07-29 15:06:30 +0200

    feat: parsers: add support for vwbank

---
+++ src/parsers/banks/vwbank.py
@@ -0,0 +1,24 @@
+"""Parser for Volkswagen Bank CSV exports."""
+
+from pathlib import Path
+from typing import override
+
+import pandas as pd
+
+from parsers import REGISTRY, Parser
+
+
+@REGISTRY.register
+class VWBankParser(Parser):
+    """Parser class for Volkswagen Bank CSV exports."""
+
+    TYPE = "vwbank"
+    FTYPE = "csv"
+
+    @override
+    def read_file(self, fpath: Path) -> pd.DataFrame:
+        """Read a VW Bank CSV export and return a DataFrame."""
+        df = pd.read_csv(fpath, header=6, sep=";")
+        df = df.dropna(how="all", axis=1)
+        df["dt"] = pd.to_datetime(df["Buchungsdatum"], format="%d.%m.%Y")
+        return df
